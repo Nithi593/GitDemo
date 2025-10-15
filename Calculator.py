61247f0e0917bfc328f58acae812a6d61eb9ce46
Nithi593 <nithish.present@gmail.com>
2025-10-15 16:26:11 -0400

    Commit from Asian Time

--- Calculator.py
+++ Calculator.py
@@ -16,7 +16,13 @@ class BasicCalculator:
     def division(self):
         return self.a / self.b
 
+
 obj = BasicCalculator(10, 5)
+
+print(f"Addition: {obj.addition()}")
+print(f"Subtraction: {obj.subtraction()}")
+print(f"Multiplication: {obj.multiplication()}")
+print(f"Division: {obj.division()}")
 
 print(f"Addition: {obj.addition()}")
 print(f"Subtraction: {obj.subtraction()}")
